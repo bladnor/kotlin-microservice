2a8bb723c552f1cf2732cd1d33c73b33e93ba68c
Roland Berger <roland.berger@exasoft.ch>
2016-08-26 12:54:38 +0200

    Initial commit

---
+++ src/main/java/ch/sbb/devday2016/KotlinMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package ch.sbb.devday2016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinMicroserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/ch/sbb/devday2016/KotlinMicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.sbb.devday2016;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KotlinMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
